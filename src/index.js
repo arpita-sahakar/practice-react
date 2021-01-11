a77e06a1b39eec2034c34195a2c39289b98e5dda
Arpita Saha <arpita.sahaa@gmail.com>
2021-01-10 21:29:39 -0500

    activity 03

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
+//render the app container in "root" div. The "root" div is located in index.html
 ReactDOM.render(<App />, document.getElementById("root"));
