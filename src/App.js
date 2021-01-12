f37e25dc18a8d6809c306369a7147e1509f21949
Arpita Saha <arpita.sahaa@gmail.com>
2021-01-11 19:00:52 -0500

    activity 10

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import React from "react";
-import Header from "./components/Header";
-import Section from "./components/Section";
-
-import Navbar from "./components/Navbar";
+import Alert from "./components/Alert";
 
 function App() {
-  return (
-    <div>
-      <Navbar />
-      <Header />
-      <Section />
-    </div>
-  );
+  return <Alert type="danger">Invalid user id or password</Alert>;
 }
 
 export default App;
