d41d928d27af24bc18a24f0cc937ac6a6963ae70
Arpita Saha <arpita.sahaa@gmail.com>
2021-01-13 13:04:23 -0500

    activity 15

--- src/components/CounterFunc.jsx
+++ src/components/CounterFunc.jsx
@@ -1,30 +1,32 @@
 import React, { useState } from "react";
-//This says that we will use State Hook for the current component
 
-const CounterFunc = (props) => {
-  //Below the first index of array is variable whose state we want to capture
-  //second is a function to update the variable
+const Counter = (props) => {
+    // count is a variable , setCount is the setter of that variable, 
+    // In this case useState defines the initial value of the count
   const [count, setCount] = useState(0);
 
-  // handleIncrement increments this.state.count by 1
   const handleIncrement = () => {
-    // We always use the setState method to update a component's state
     setCount(count + 1);
   };
 
+  const handleDecrement = () => {
+    setCount(count - 1);
+  };
   return (
     <div className="card text-center">
-      <div className="card-header bg-primary text-white">
-        Click Counter!
-      </div>
+      <div className="card-header bg-primary text-white">Click Counter!</div>
       <div className="card-body">
         <p className="card-text">Click Count: {count}</p>
         <button className="btn btn-primary" onClick={handleIncrement}>
           Increment
+        </button>
+
+        <button className="btn btn-primary" onClick={handleDecrement}>
+          Decrement
         </button>
       </div>
     </div>
   );
 };
 
-export default CounterFunc;
+export default Counter;
