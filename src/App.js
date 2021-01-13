ee5ab719c9d541f8239cf039e54232f8d96fa8dd
Arpita Saha <arpita.sahaa@gmail.com>
2021-01-13 18:14:24 -0500

    activity 17

--- src/App.js
+++ src/App.js
@@ -1,33 +1,8 @@
-import FriendCard from "./components/FriendCard";
-import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
-import friends from "./friends.json";
-import React, { useState } from "react";
+import React from "react";
+import FormFunc from "./components/Form/FormFunc.jsx";
 
 function App() {
-  const [friendArrayState, setFriendArrayState] = useState(friends);
-  console.log(friends);
-  console.log(friendArrayState);
-
-  // const createFriendCard = () => {
-    // for every frnd in friendArrayState create friendCard with name, image,occupation,location as props.
-    const friendCardArray = friendArrayState.map((frnd) => (
-      <FriendCard
-        name={frnd.name}
-        image={frnd.image}
-        occupation={frnd.occupation}
-        location={frnd.location}
-      />
-    ));
-    // return friendCardArray;
-  // };
-
-  return (
-    <Wrapper>
-      <Title>Friends List</Title>
-      {friendCardArray}
-    </Wrapper>
-  );
+  return <FormFunc />;
 }
 
 export default App;
