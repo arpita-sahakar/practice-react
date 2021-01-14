b9d3c4a5bcc24250bfa5141921b5c7d228d725c3
Arpita Saha <arpita.sahaa@gmail.com>
2021-01-13 21:02:24 -0500

    update

--- src/components/CounterFunc.jsx
+++ src/components/CounterFunc.jsx
@@ -1,27 +1,33 @@
 import React, { useState } from "react";
+//This says that we will use State Hook for the current component
 
-const Counter = (props) => {
-    // count is a variable , setCount is the setter of that variable, 
-    // In this case useState defines the initial value of the count
+const CounterFunc = (props) => {
+  //Below the first index of array is variable whose state we want to capture
+  //second is a function to update the variable
   const [count, setCount] = useState(0);
 
+  // handleIncrement increments this.state.count by 1
   const handleIncrement = () => {
+    // We always use the setState method to update a component's state
     setCount(count + 1);
   };
 
   const handleDecrement = () => {
+    // We always use the setState method to update a component's state
     setCount(count - 1);
   };
+
   return (
     <div className="card text-center">
-      <div className="card-header bg-primary text-white">Click Counter!</div>
+      <div className="card-header bg-primary text-white">
+        Click Counter!
+      </div>
       <div className="card-body">
         <p className="card-text">Click Count: {count}</p>
         <button className="btn btn-primary" onClick={handleIncrement}>
           Increment
         </button>
-
-        <button className="btn btn-primary" onClick={handleDecrement}>
+        <button className="btn btn-danger" onClick={handleDecrement}>
           Decrement
         </button>
       </div>
@@ -29,4 +35,4 @@ const Counter = (props) => {
   );
 };
 
-export default Counter;
+export default CounterFunc;
